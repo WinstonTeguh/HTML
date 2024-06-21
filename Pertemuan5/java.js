90e5784f579603721b33c5f78ad0e09ec524fe7a
WinstonTeguh <tghwinston@gmail.com>
2024-06-21 18:57:19 +0700

    Add files via upload

---
+++ Pertemuan5/java.js
@@ -0,0 +1,11 @@
+function changeColor(color) {
+    document.getElementById('myTable').style.backgroundColor = color;
+}
+
+function myfunction() {
+    alert ("Hello!!") ;
+}
+
+function myFunction1() {
+    confirm("Press a button!");
+  }
